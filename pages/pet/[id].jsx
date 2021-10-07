54540d416c1e14826deecdd2def08c9ffd1b7f51
Bruno Moraes <bmoraes@registro.br>
2021-10-07 19:59:33 -0300

    Detail page layout

--- pages/pet/[id].jsx
+++ pages/pet/[id].jsx
@@ -1,4 +1,5 @@
 import Container from '../../components/container'
+import styles from '../../styles/Pet.module.scss'
 import { useRouter } from 'next/router'
 
 export default function PetItem() {
@@ -7,7 +8,19 @@ export default function PetItem() {
 
 	return (
 		<Container>
-			pet: {id}
+			<section className={styles.highlight}>
+				<img src="/dog.jpg" alt="nome_do_pet" />
+				<div>
+					<img src="/images/male.png" />
+					<h1>Dino</h1>
+					<small>3 anos</small>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur est et tempore. Doloribus cupiditate laudantium obcaecati accusantium accusamus, possimus similique quia blanditiis deleniti qui unde ullam facilis, ad repellat iste, culpa nesciunt, minus. Eaque, omnis, iure! Eveniet culpa at, nostrum reprehenderit. Aliquam dolorem delectus ratione labore dignissimos, mollitia dolore, voluptates.</p>
+
+					<a className="bt" href title="Tenho interesse em adotar">Tenho interesse no Dino!</a>
+				</div>
+			</section>
+
+			<a className={[styles.bt_back, 'bt', 'bt_brown'].join(' ')} href="/" title="Visualizar lista">Voltar e visualizar outros pets</a>
 		</Container>
 	)
 }
