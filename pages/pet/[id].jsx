4be6e9bcdf6df78ed181cc7b7feb79ae6511d28e
Bruno Moraes <bmoraes@registro.br>
2021-10-08 20:31:40 -0300

    Detail page request

--- pages/pet/[id].jsx
+++ pages/pet/[id].jsx
@@ -1,22 +1,27 @@
 import Container from '../../components/container'
 import styles from '../../styles/Pet.module.scss'
-import { useRouter } from 'next/router'
+import axios from 'axios'
 
-export default function PetItem() {
-	const router = useRouter()
-	const {id} = router.query
+axios.get('https://adotappet-api.herokuapp.com/api/pets/34').then(({ data }) => {
+  console.log('oi', data)
+})
 
+const PetItem = ({ pet, error }) => {
 	return (
 		<Container>
 			<section className={styles.highlight}>
-				<img src="/dog.jpg" alt="nome_do_pet" />
+				<img src={pet.imagem} alt={pet.nome} />
 				<div>
-					<img src="/images/male.png" />
-					<h1>Dino</h1>
-					<small>3 anos</small>
+					<img src={pet.sexo == 'MASCULINO' ? '/images/male.png' : '/images/female.png'} />
+					<h1>{pet.nome}</h1>
+					<small>{dateFormat(pet.dataNascimento)}</small>
+
+					<p><strong>Porte: </strong>{pet.porte}</p>
+					<p><strong>Raça: </strong>{pet.raca}</p>
+					<br />
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur est et tempore. Doloribus cupiditate laudantium obcaecati accusantium accusamus, possimus similique quia blanditiis deleniti qui unde ullam facilis, ad repellat iste, culpa nesciunt, minus. Eaque, omnis, iure! Eveniet culpa at, nostrum reprehenderit. Aliquam dolorem delectus ratione labore dignissimos, mollitia dolore, voluptates.</p>
 
-					<a className="bt" href="#" title="Tenho interesse em adotar">Tenho interesse no Dino!</a>
+					<a className="bt" href="#" title="Tenho interesse em adotar">{'Tenho interesse n' + (pet.sexo == 'MASCULINO' ? 'o ' : 'a ') + pet.nome + '!'}</a>
 				</div>
 			</section>
 
@@ -24,3 +29,23 @@ export default function PetItem() {
 		</Container>
 	)
 }
+
+PetItem.getInitialProps = async ({ query }) => {
+	try {
+		const res = await axios.get('https://adotappet-api.herokuapp.com/api/pets/' + query.id);
+		const pet = res.data;
+
+		return { pet };
+	} catch (error) {
+		return { error };
+	}
+}
+
+const dateFormat = (date) => {
+	const dateTimeFormat = new Intl.DateTimeFormat('pt-BR', { year: 'numeric', month: 'numeric', day: 'numeric', })
+	const format = (d) => dateTimeFormat.format(new Date(d))
+
+	return 'Nasceu em ' + format(date)
+}
+
+export default PetItem
